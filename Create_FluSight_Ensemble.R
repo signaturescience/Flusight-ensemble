396b5ed87633e842791b6e6617c665dd6fe01a4d
AWebber1 <87338444+AWebber1@users.noreply.github.com>
2022-05-09 10:31:30 -0400

    Update Create_FluSight_Ensemble.R

--- Create_FluSight_Ensemble.R
+++ Create_FluSight_Ensemble.R
@@ -16,8 +16,8 @@ library(dplyr)
 
 # Set the environment - dates should change each week & check to see if the file paths are correct 
 userid = "rpe5"
-forecast_date = "2022-04-25" # Monday
-sixweeks_before_forecast_date = "2022-03-14" # 6 weeks ago Monday
+forecast_date = "2022-05-02" # Monday
+sixweeks_before_forecast_date = "2022-03-21" # 6 weeks ago Monday
 
 ensemble_code_path = paste0("C:/Users/",userid,"/Desktop/GitHub/Flusight-ensemble")
 flusight_path = paste0("C:/Users/",userid,"/Desktop/GitHub/Flusight-forecast-data") #using my forked repo for this now
